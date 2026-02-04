c83724860ce1ce951c553b38c1367e6eb73f948a
JasirV <jasirmon133@gmail.com>
2026-02-04 21:40:45 +0530

    fix: ESM exports, dirname, and public API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import countries from './data/countries.json' assert { type: "json" };
-import { validatePhoneNumber } from './validators/phoneValidator.js';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import countries from "./data/countries.json" assert { type: "json" };
 
-export function getCountries() {
+import { validatePhoneNumber } from "./validators/phoneValidator.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function getAllCountries() {
   return countries;
 }
 
@@ -11,10 +16,10 @@ export function getFlagSvg(iso2) {
   try {
     const filePath = path.join(
       __dirname,
-      'flags',
+      "flags",
       `${iso2.toUpperCase()}.svg`
     );
-    return fs.readFileSync(filePath, 'utf8');
+    return fs.readFileSync(filePath, "utf8");
   } catch {
     return null;
   }
