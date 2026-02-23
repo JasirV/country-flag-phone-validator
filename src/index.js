0c580dc4ad546be5274e6182f1565c215cdf555c
JasirV <jasirmon133@gmail.com>
2026-02-23 14:42:22 +0530

    update code some erros

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
+
 import countries from "./data/countries.json" assert { type: "json" };
 
 import { validatePhoneNumber } from "./validators/phoneValidator.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export function getAllCountries() {
   return countries;
 }
 
 export function getFlagSvg(iso2) {
+  if (!iso2) return null;
+
   try {
-    const filePath = path.join(
-      __dirname,
-      "flags",
-      `${iso2.toUpperCase()}.svg`
-    );
-    return fs.readFileSync(filePath, "utf8");
+    return new URL(`./flags/${iso2.toUpperCase()}.svg`, import.meta.url).href;
   } catch {
     return null;
   }
